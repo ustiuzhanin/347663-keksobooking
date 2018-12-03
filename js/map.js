300186a9670ae78ef4b820923fd62ccbbe2c7b34
Slava <ucheba00@gmail.com>
2018-12-03 16:08:48 -0700

    fix travis

--- js/map.js
+++ js/map.js
@@ -282,25 +282,25 @@ var stayingPrice = document.querySelector('#price');
 var onStayingChange = function () {
   switch (stayingType.value) {
     case 'bungalo':
-    stayingPrice.min = 0;
-    stayingPrice.placeholder = 0;
-    break;
+      stayingPrice.min = 0;
+      stayingPrice.placeholder = 0;
+      break;
     case 'flat':
-    stayingPrice.min = 1000;
-    stayingPrice.placeholder = 1000;
-    break;
+      stayingPrice.min = 1000;
+      stayingPrice.placeholder = 1000;
+      break;
     case 'house':
-    stayingPrice.min = 5000;
-    stayingPrice.placeholder = 5000;
-    break;
+      stayingPrice.min = 5000;
+      stayingPrice.placeholder = 5000;
+      break;
     case 'palace':
-    stayingPrice.min = 10000;
-    stayingPrice.placeholder = 10000;
-    break;
+      stayingPrice.min = 10000;
+      stayingPrice.placeholder = 10000;
+      break;
   }
 };
 
-stayingType.addEventListener('change', onStayingChange)
+stayingType.addEventListener('change', onStayingChange);
 
 /* checkin/out restrictions */
 
@@ -315,7 +315,7 @@ var onCheckInOutChange = function (evt) {
   } else {
     evt.target.setCustomValidity('');
   }
-}
+};
 
 checkin.addEventListener('change', onCheckInOutChange);
 checkout.addEventListener('change', onCheckInOutChange);
@@ -325,13 +325,13 @@ checkout.addEventListener('change', onCheckInOutChange);
 var numberOfRooms = document.querySelector('#room_number');
 var numberOfGuests = document.querySelector('#capacity');
 
-var guestsArr = Array.from(numberOfGuests.children)
+var guestsArr = Array.from(numberOfGuests.children);
 var guests = {
   1: 3,
   2: 2,
   3: 1,
   100: 4
-}
+};
 
 var setGuestsCapacity = function (rooms) {
 
@@ -345,38 +345,38 @@ var setGuestsCapacity = function (rooms) {
 
   if (roomsNumber !== 100) {
     for (var i = 1; i <= roomsNumber; i++) {
-      guestsArr[guests[i]].disabled = false
+      guestsArr[guests[i]].disabled = false;
     }
   } else {
-    guestsArr[guests[100]].disabled = false
+    guestsArr[guests[100]].disabled = false;
   }
   /* set the value to none after changing the staying type */
   numberOfGuests.value = '';
-}
+};
 
-var onRoomChange = function (evt) {
+var onRoomChange = function () {
 
   switch (numberOfRooms.value) {
     case '1':
-    setGuestsCapacity(numberOfRooms.value);
-    break;
+      setGuestsCapacity(numberOfRooms.value);
+      break;
 
     case '2':
-    setGuestsCapacity(numberOfRooms.value);
-    break;
+      setGuestsCapacity(numberOfRooms.value);
+      break;
 
     case '3':
-    setGuestsCapacity(numberOfRooms.value);
-    break;
+      setGuestsCapacity(numberOfRooms.value);
+      break;
 
     case '100':
-    setGuestsCapacity(numberOfRooms.value);
-    break;
+      setGuestsCapacity(numberOfRooms.value);
+      break;
   }
   if (numberOfGuests.value === '4') {
     numberOfGuests.setCustomValidity('Выберите количество гостей');
   }
-}
+};
 onRoomChange();
 
-numberOfRooms.addEventListener('change', onRoomChange)
+numberOfRooms.addEventListener('change', onRoomChange);
